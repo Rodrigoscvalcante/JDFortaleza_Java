8b1205db49b4a197240dc228f54ee5ad9430d9f4
Rodrigoscvalcante <112738428+Rodrigoscvalcante@users.noreply.github.com>
2022-09-11 12:02:15 -0300

    Arquivo Inicial do Projeto
    Entrada e saída de dados.

---
+++ Ola.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ola {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Digite seu nome: ");	
+	
+	Scanner leitor = new Scanner(System.in);
+	
+	String variavel;
+	
+	variavel = leitor.nextLine();
+	
+	System.out.println("Olá " +variavel+ " digite sua Idade: ");	
+	
+	
+	int idade;
+	idade = leitor.nextInt();
+	
+	System.out.println("Olá " +variavel+ " sua idade é: " +idade);
+		
+	}
+
+}
+		
